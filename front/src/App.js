18e73f0dc0d7414e4c81cc337251ef278358dbe6
윤자현 <bajh323@naver.com>
2023-07-27 13:34:09 +0900

    background updated yjh

--- front/src/App.js
+++ front/src/App.js
@@ -1,43 +1,50 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Header from './Header';
-import Footer from './Footer';
-import Home from './Home';
-import NotFound from './NotFound';
-import Fishing from './Fishing';
-import Login from './account/Login';
-import Signup from './account/Signup';
-
-
-
+import Footer from "./Footer";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import Fishing from "./Fishing";
+import Login from "./account/Login";
+import Signup from "./account/Signup";
+import Background from "./Background";
+import Books from "./Books";
 // import { useNavigate } from 'react-router-dom';
 // button onClick={() => navigate('/')}>Go Root</button>
 
 function App(props) {
-	
   return (
-		<div className='App' style={{
-      margin: 'auto',
-    //   width: '80%',
-    }}>
-			<BrowserRouter>
-				{/* <Header style={{
+    <div
+      className="App"
+      style={{
+        margin: "auto",
+        //   width: '80%',
+      }}
+    >
+      {" "}
+      <BrowserRouter>
+        {/* <Header style={{
               margin:'auto',
              }}/> */}
-				<Routes style={{
-              margin:'auto',
-             }}>
-					<Route path="/" element={<Home />}></Route>
-					<Route path="/fishing" element={<Fishing />}></Route>
-					<Route path="/Login" element={<Login />}></Route>
-					<Route path="/Signup" element={<Signup />}></Route>
-					{/* <Route path="/product/:productId" element={<Login />}></Route> */}
-					<Route path="*" element={<NotFound />}></Route>
-				</Routes>
-				<Footer/>
-			</BrowserRouter>
-		</div>
+        <Background />
+        <Routes
+          style={{
+            margin: "auto",
+          }}
+        >
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/fishing" element={<Fishing />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/Signup" element={<Signup />}></Route>
+          <Route path="/Books" element={<Books />}></Route>
+          {/* <Route path="/product/:productId" element={<Login />}></Route> */}
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
