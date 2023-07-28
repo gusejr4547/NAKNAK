1044f0f25459108428e19113364b7e47dbc0ed9b
윤자현 <bajh323@naver.com>
2023-07-28 15:22:03 +0900

    footer and loading page
    Former-commit-id: 03f0fbfb95dcc663df08db9c1da67a61e7a97a18

--- front/src/Background.js
+++ front/src/Background.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Background.css";
+import { useRecoilState } from "recoil";
+import { fishingMode_recoil } from "./atoms";
+import { Link } from "react-router-dom";
 
 const Background = () => {
+  const [activeNav, setActiveNav] = useState(1);
+  const [fishingMode, setFishingMode] = useRecoilState(fishingMode_recoil);
+
   return (
     <span>
+      <div className={fishingMode === "selectMode" ? "hidden" : "fishingMode"}>
+        <Link
+          to="/fishing"
+          className="nav-link"
+          onClick={() => setActiveNav(2)}
+        >
+          피싱모드가 진행 중입니다. 클릭하시면 피싱으로 갑니다.
+        </Link>
+      </div>
       <img src="../assets/cat.png" alt="" className="cat" />
       <img src="../images/island.png" alt="" className="island" />
     </span>
