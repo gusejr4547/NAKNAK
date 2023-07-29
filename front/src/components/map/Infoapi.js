17b9cbb2fbdd5b84a936341ac365dbc8877c1493
최상익 <qjxltm@naver.com>
2023-07-30 02:34:43 +0900

    20230730_지도api받기수정
    Former-commit-id: 72b1a87fa11dd77204b662386222d152c24d01ef

--- front/src/components/map/Infoapi.js
+++ front/src/components/map/Infoapi.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
-import axios from "../../api/SeaAPI";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import axios from "../../api/SeaAPI";
 
 function Infoapi(props) {
+  const [data, setData] = useState(null);
+
   const fetchData = async (props) => {
     try {
       console.log(props);
       const response = await axios.get(
-        `openWeatherNow.do?mmaf=${props.mmaf}&mmsi=${props.mmsi}`
-        // {
-        //   withCredentials: true, // CORS 이슈를 해결하기 위해 자격 증명 포함
-        // }
+        `/api2/openWeatherNow.do?mmaf=${props.mmaf}&mmsi=${props.mmsi}&serviceKey=102D1304-985C-4C11-89D1-574914365F64&resultType=json`
       );
+      // const response = await axios.get(
+      //   `/api2/openWeatherNow.do?mmaf=101&mmsi=994401578&serviceKey=102D1304-985C-4C11-89D1-574914365F64&resultType=json`
+      // );
       console.log("성공");
-      console.log(response.data);
+      console.log(response)
+      console.log(response.data.result.recordset);
+      setData(response.data.result.recordset);
     } catch (e) {
       console.log(e);
     }
@@ -23,6 +28,12 @@ function Infoapi(props) {
       <button onClick={() => fetchData({ mmaf: 101, mmsi: 994401578 })}>
         클릭
       </button>
+      {data && (
+        <div>
+          <h2>결과:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
