a63e08fc5e6167d3c7cbdc3d52f99556f8e14646
조현덕 <gusejr4547@gmail.com>
2023-07-31 11:45:27 +0900

    Fix: os 종속성 변경
    Former-commit-id: 00284f56136504bd090e46bda0b3bc70102a372d

--- back/Django/fish/fish/settings.py
+++ back/Django/fish/fish/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(3s!+ib8hscw7m7&_&tk0i#q_xbh7^^wnkamwcms@9^p8gb^p*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
