d550f613d8f0d41ba3458719aad5dc5e017b0692
윤자현 <bajh323@naver.com>
2023-07-31 17:57:45 +0900

    0731 지도에 핀박기 모달 수정 및 api 가져오기
    Former-commit-id: 25766cbcb44c79597a28f50aa55153c2a2634e4a

--- front/src/components/map/MapModal.js
+++ front/src/components/map/MapModal.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./MapModal.css";
 import { useRecoilState } from "recoil";
-import { mapModal_recoil } from "../../utils/atoms";
+import { mapModal_recoil, fishingInfo_recoil } from "../../utils/atoms";
 
-const MapModal = (markerPosition) => {
+const MapModal = () => {
   const [modalOpen, setModalOpen] = useRecoilState(mapModal_recoil);
+  const [data, setData] = useRecoilState(fishingInfo_recoil);
+
+  useEffect(() => {
+    // 근데 콘솔창이 두번찍히네..
+    console.log(data);
+
+    return () => {};
+  }, [data]);
+
   return (
     <div className="presentation" role="presentation">
       <div className="modal_wrap">
@@ -13,12 +22,18 @@ const MapModal = (markerPosition) => {
           X
         </span>
         <div className="modal"></div>
-        <h1>모달이다 이놈아</h1>
-        <div>여기는 : `{markerPosition.content}`</div>
-        <div>
-          여기는 위치는: 경도 {markerPosition.latlng.La} 위도{" "}
-          {markerPosition.latlng.Ma}
+        <div className="modal-title">{data[0].MMSI_NM}</div>
+        <div className="modal-information">
+          <p>습도: {data[0].HUMIDITY}</p>
+          <p>기온: {data[0].AIR_TEMPERATURE}</p>
+          <p>위도: {data[0].LATITUDE}</p>
+          <p>경도: {data[0].LONGITUDE}</p>
+          <p>풍향: {data[0].WIND_DIRECT}</p>
+          <p>기압: {data[0].AIR_PRESSURE}</p>
+          <p>풍속: {data[0].WIND_SPEED}</p>
+          <p>수온: {data[0].WATER_TEMPER}</p>
         </div>
+
         {/* <h2>{marker}</h2> */}
         <div className="modal__content">
           <p className="modal__details">
--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -1,25 +1,24 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // 프록시1 설정
   app.use(
-    '/api',
+    "/api",
     createProxyMiddleware({
-      target: 'http://192.168.30.161:8080',
+      target: "http://192.168.30.161:8080",
       changeOrigin: true,
     })
   );
 
   // 프록시2 설정
   app.use(
-    '/api2',
+    "/api2",
     createProxyMiddleware({
-      target: 'http://marineweather.nmpnt.go.kr:8001',
+      target: "http://marineweather.nmpnt.go.kr:8001",
       changeOrigin: true,
       pathRewrite: {
-        '^/api2': '',
+        "^/api2": "",
       },
     })
   );
-
 };
