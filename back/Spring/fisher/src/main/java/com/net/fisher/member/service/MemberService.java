3d7cbc2d7dcd4429ceb36738340e94d722da6608
이강민 <ygangmin@gmail.com>
2023-08-01 10:25:36 +0900

    Fix : test1
    Former-commit-id: 87fdba966ea1d2eff55c5be22d2eaa158a2bceab

--- back/Spring/fisher/src/main/java/com/net/fisher/member/service/MemberService.java
+++ back/Spring/fisher/src/main/java/com/net/fisher/member/service/MemberService.java
@@ -63,7 +63,7 @@ public class MemberService {
     @PostConstruct
     public void initialize(){ // server on mattermost bot
         String ipAddressStr = "ip"; //test
-        try {//test2
+        try {
             InetAddress ipAddress = InetAddress.getLocalHost();
             ipAddressStr = ipAddress.getHostAddress();
             System.out.println("Your IP address is: " + ipAddressStr);
