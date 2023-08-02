b015460fea80cdc08a0035434d1eb690a710b408
조병철 <zxas1245@naver.com>
2023-08-02 14:33:24 +0900

    [FE] 네비게이션 바 css 이름 변경
    외부 라이브러리 사용 시 css 클래스명 겹침 문제 예방을 위해 nav css
    클래스 명 앞에 nav- 다 추가했습니다.
    
    
    Former-commit-id: 377a0db32698583256d5eac94542f9a8924d95d5

--- front/src/components/board/Board.jsx
+++ front/src/components/board/Board.jsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { authorizedRequest } from "../account/AxiosInterceptor";
-
+import "./Board.css";
 const Board = () => {
-  return (
-    <div>
-      <div>here is board</div>
-    </div>
-  );
+  return <div className="board-wrapper"></div>;
 };
 
 export default Board;
