0a2d50caa0073cb00828baf212d65a9138d1a6da
윤자현 <bajh323@naver.com>
2023-08-04 00:11:12 +0900

    중간 저장....뉴비 대화창 만드는 중 ..  1일 1커밋 안했다... 난 바보야
    Former-commit-id: 95dc0a79bda16d99aa3dd9d1eafd197f9472d470

--- front/src/components/freshman/Firstpage.js
+++ front/src/components/freshman/Firstpage.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Wave from "react-wavify";
 import "./Firstpage.css";
+import talk from "./Talk";
 
 function Firstpage() {
+  const [step, setStep] = useState(0);
+
+  const btn1 = () => {
+    if (step === 0) {
+      setStep(1);
+    }
+  };
+  const btn2 = (i) => {
+    if (step === 0) {
+      setStep(1);
+    }
+  };
+
   return (
     <div className="first_wrapper">
-      {/* <div className="first_malpoong"></div> */}
+      <div className="first_talk">
+        <span className="first_title">
+          {talk[step].content}
+          {/* {talk.map((item) => {
+            return (
+              <>
+                {item.id}
+                {item.content}
+              </>
+            );
+          })} */}
+        </span>
+      </div>
       <div className="first_wave_wrap">
-        <div className="first_title">
-          <span>안녕 낙낙월드에 처음왔지? 내가 몇가지 물어볼게!</span>
-        </div>
         <div className="first_island_img"></div>
         <div className="first_cat_img"></div>
         {/* 가장 가까운 파도 */}
@@ -52,8 +75,12 @@ function Firstpage() {
           }}
         />
         <div className="btns">
-          <span className="btn">응</span>
-          <span className="btn">아니</span>
+          <span className="answer1" onClick={() => btn1()}>
+            {talk[step].answer1}
+          </span>
+          <span className="answer2" onClick={() => btn2()}>
+            {talk[step].answer2}
+          </span>
         </div>
       </div>
     </div>
