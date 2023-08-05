305e4e460fa94f782b7eb57945438e8028410f2a
DryLeonhard <ygangmin@gmail.com>
2023-08-05 15:22:39 +0900

    Refactor : 주석 설명 추가
    Former-commit-id: 9b51e1c53d52418238076de875317d9a7fb34637

--- back/Spring/fisher/src/main/java/com/net/fisher/config/SecurityConfiguration.java
+++ back/Spring/fisher/src/main/java/com/net/fisher/config/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeHttpRequests(authorize->authorize
                         //.requestMatchers(HttpMethod.POST,"/h2-console/*").permitAll()
-                        .requestMatchers(toH2Console()).permitAll()
+                        .requestMatchers(toH2Console()).permitAll() // H2 Console 에 대한 모든 접근을 허용하기 위한 메서드
                         //.requestMatchers(HttpMethod.POST,"/h2-console/*").permitAll()
                         .requestMatchers("/oauth2/*").permitAll()
                         .requestMatchers(HttpMethod.POST,"/api/login").permitAll()
