c8a305bd5a373f83181fb4d7f9e7c5e47f0e460c
조현덕 <gusejr4547@gmail.com>
2023-08-09 00:37:30 +0900

    Feat: 댓글작성
    Former-commit-id: 3ebb8810abdad899b01c5149fc9f3d54db83c409

--- back/Spring/fisher/src/main/java/com/net/fisher/comment/controller/CommentController.java
+++ back/Spring/fisher/src/main/java/com/net/fisher/comment/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.net.fisher.comment.controller;
 
 import com.net.fisher.auth.jwt.JwtTokenizer;
 import com.net.fisher.comment.dto.CommentDto;
+import com.net.fisher.comment.entity.Comment;
 import com.net.fisher.comment.mapper.CommentMapper;
 import com.net.fisher.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,15 +22,26 @@ public class CommentController {
     private final CommentMapper commentMapper;
 
     @PostMapping("/comments/{post-id}")
-    public ResponseEntity postComment(
+    public ResponseEntity<Comment> postComment(
             @PathVariable("post-id") long postId,
             @RequestHeader(name = "Authorization") String token,
             @RequestBody CommentDto.Post commentPostDto) {
 
         long tokenId = jwtTokenizer.getMemberId(token);
 
-        commentService.addComment(tokenId, postId, commentPostDto);
+        long commentId = commentService.addComment(tokenId, postId, commentPostDto);
+
+        return new ResponseEntity(commentId, HttpStatus.OK);
+    }
+
+    @GetMapping("/comments/{post-id}")
+    public ResponseEntity getComment(
+            @RequestParam("comment-id") long commentId,
+            @PathVariable("post-id") long postId){
+
+        List<Comment> commentList = commentService.getComment(postId, commentId);
 
         return new ResponseEntity(HttpStatus.OK);
     }
+
 }
