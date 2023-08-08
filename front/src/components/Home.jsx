91bad62ac01d96130a839ca13e34af3c401d510b
최상익 <qjxltm@naver.com>
2023-08-08 17:42:03 +0900

    [FE]병합후 오류 수정
    Former-commit-id: 4c17aa611a421fb91ca892e1007facc60391d3ef

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -68,8 +68,17 @@ function Home({ newbieVersion }) {
 
           {/* slide unit start*/}
           {/* <div className="home-slide"> */}
-          <div className={`home-slide ${newbieVersion ? "non-clickable" : ""}`}>
-            <Link to={`/Profile/:${userData.memberId}`} className="nav-link">
+          {userData?.memberId !== undefined ? (
+            <div
+              className={`home-slide ${newbieVersion ? "non-clickable" : ""}`}
+            >
+              <Link to={`/Profile/:${userData.memberId}`} className="nav-link">
+                <img src="/assets/icons/pro.PNG" alt="icon" />
+                <h6>프로필</h6>
+              </Link>
+            </div>
+          ) : (
+            <div className="home-slide">
               <img src="/assets/icons/pro.PNG" alt="icon" />
               <h6>프로필</h6>
             </div>
@@ -109,7 +118,7 @@ function Home({ newbieVersion }) {
           </div> */}
           {/* <div className="home-slide"> */}
           <div className={`home-slide ${newbieVersion ? "non-clickable" : ""}`}>
-            <Link to="/SeaScene" className="nav-link">
+            <Link to="/Balls" className="nav-link">
               <img src="/assets/icons/tank.PNG" alt="icon" />
               <h6>수조</h6>
             </Link>
