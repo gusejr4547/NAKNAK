1ae686b70756108df917cca76252d465c8e50297
DryLeonhard <ygangmin@gmail.com>
2023-08-10 02:05:31 +0900

    [BE] Test
    Former-commit-id: fd95327e723867ea589f0a0f4ae9074a9c18dbf3

--- back/Spring/fisher/src/main/java/com/net/fisher/config/MvcConfiguration.java
+++ back/Spring/fisher/src/main/java/com/net/fisher/config/MvcConfiguration.java
@@ -9,12 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/static/js/").setCachePeriod(60 * 60 * 24 * 365);
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/static/css/").setCachePeriod(60 * 60 * 24 * 365);
-        registry.addResourceHandler("/media/**").addResourceLocations("classpath:/static/static/media/").setCachePeriod(60 * 60 * 24 * 365);
-        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/").setCachePeriod(60 * 60 * 24 * 365);
-        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/").setCachePeriod(60 * 60 * 24 * 365);
-        registry.addResourceHandler("/model/**").addResourceLocations("classpath:/static/model/").setCachePeriod(60 * 60 * 24 * 365);
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/static/css/");
+        registry.addResourceHandler("/media/**").addResourceLocations("classpath:/static/static/media/");
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+        registry.addResourceHandler("/model/**").addResourceLocations("classpath:/static/model/");
 
     }
 }
