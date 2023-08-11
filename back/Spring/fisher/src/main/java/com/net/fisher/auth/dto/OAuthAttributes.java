a9aae31c0462b0676d4ce9a52ed22c4d3f8134ec
maknoon9002 <maknoon9002@gmail.com>
2023-08-11 10:10:23 +0900

    [OPS] origin add
    Former-commit-id: e7757ce9e05a94eae84505a9e5fea78cfb103644

--- back/Spring/fisher/src/main/java/com/net/fisher/auth/dto/OAuthAttributes.java
+++ back/Spring/fisher/src/main/java/com/net/fisher/auth/dto/OAuthAttributes.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
-    private String nameAttributeKey;
+    private String nameAttributeKey;F
     private String name;
     private String email;
     private String picture;
