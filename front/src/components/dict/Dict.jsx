e799410acf851fd4e8e334fdbc49694ecd4fb194
최상익 <qjxltm@naver.com>
2023-08-11 12:44:02 +0900

    [FE]FIX_위치정보수정
    Former-commit-id: 57f8d359f0deafa624df7ec8162ec002c9c42125

--- front/src/components/dict/Dict.jsx
+++ front/src/components/dict/Dict.jsx
@@ -6,6 +6,7 @@ import {
   manner,
   limit_length,
   action,
+  limit_date,
 } from "../../utils/data/point";
 import Dictlist from "./Dictlist";
 import "./Dict.css";
@@ -15,14 +16,99 @@ import { location_recoil } from "../../utils/atoms";
 function Dict(props) {
   const [activeView, setActiveView] = useState("");
   const [location, setLocation] = useRecoilState(location_recoil);
+  const [inputData, setinputData] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [lodata, setlodata] = useState("");
+
+  const getlocation = () => {
+    console.log(123);
+    // 리액트 웹 앱의 JavaScript 코드
+    window.addEventListener("message", (event) => {
+      try {
+        const data = JSON.parse(event.data);
+        console.log("Received data from Flutter:", data);
+        // 데이터 처리를 수행합니다.
+        setlodata(data);
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      }
+    });
+    // window.flutter_inappwebview.callHandler(updatelocation, 123);
+  };
 
   const handleToggle = (view) => {
     setActiveView(view);
   };
 
+  const Search = (event) => {
+    if (event.key === "Enter") {
+      setSearchData([]);
+      const arr = [];
+      point.forEach((ele) => {
+        if (ele.title.includes(inputData)) {
+          arr.push(ele);
+          setSearchData(...searchData, arr);
+        }
+      });
+      equipment.forEach((ele) => {
+        if (ele.title.includes(inputData)) {
+          arr.push(ele);
+          setSearchData(...searchData, arr);
+        }
+      });
+      Chaebi.forEach((ele) => {
+        if (ele.title.includes(inputData)) {
+          arr.push(ele);
+          setSearchData(...searchData, arr);
+        }
+      });
+      manner.forEach((ele) => {
+        if (ele.title.includes(inputData)) {
+          arr.push(ele);
+          setSearchData(...searchData, arr);
+        }
+      });
+      limit_length.forEach((ele) => {
+        if (ele.title.includes(inputData)) {
+          arr.push(ele);
+          setSearchData(...searchData, arr);
+        }
+      });
+      action.forEach((ele) => {
+        if (ele.title.includes(inputData)) {
+          arr.push(ele);
+          setSearchData(...searchData, arr);
+        }
+      });
+      limit_date.forEach((ele) => {
+        if (ele.title.includes(inputData)) {
+          arr.push(ele);
+          setSearchData(...searchData, arr);
+        }
+      });
+      console.log(searchData);
+    } else {
+      setSearchData([]);
+      const Data = event.target.value;
+      setinputData(Data);
+    }
+  };
+
   return (
     <div>
-      <input type="text" />
+      <input
+        // className="search"
+        placeholder="궁금한 점을 검색해주세요."
+        onChange={Search}
+        onKeyPress={Search}
+      />
+      <div className="search-wrapper">
+        {searchData.map((data, index) => (
+          <p className="mapsearchresult" key={index}>
+            {data.title}
+          </p>
+        ))}
+      </div>
       <div className="dictBottom">
         <div className="dicttoggleBar" style={{ display: "flex" }}>
           <button
@@ -52,6 +138,12 @@ function Dict(props) {
             매너
           </button>
           <button
+            onClick={() => handleToggle("action")}
+            className={activeView === "action" ? "dictactive" : "dictdefault"}
+          >
+            동작
+          </button>
+          <button
             onClick={() => handleToggle("limit_length")}
             className={
               activeView === "limit_length" ? "dictactive" : "dictdefault"
@@ -60,10 +152,12 @@ function Dict(props) {
             금지체장
           </button>
           <button
-            onClick={() => handleToggle("action")}
-            className={activeView === "action" ? "dictactive" : "dictdefault"}
+            onClick={() => handleToggle("limit_date")}
+            className={
+              activeView === "limit_date" ? "dictactive" : "dictdefault"
+            }
           >
-            동작
+            금어기
           </button>
         </div>
         {activeView === "point" && (
@@ -96,13 +190,21 @@ function Dict(props) {
             <Dictlist data={action} />
           </div>
         )}
+        {activeView === "limit_date" && (
+          <div className="dictMiddle">
+            <Dictlist limit_d={limit_date} />
+          </div>
+        )}
       </div>
+      <button onClick={() => getlocation()}>123123{}</button>
       {location && (
         <p>
+          123
           {location.latitude}
           {location}
         </p>
       )}
+      {lodata && <p>{lodata.latitude}</p>}
     </div>
   );
 }
