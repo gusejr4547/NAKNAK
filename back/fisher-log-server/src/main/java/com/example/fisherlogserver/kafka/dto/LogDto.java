59ac4fd8e5c9009ea739c21db75634556e3689c5
DryLeonhard <ygangmin@gmail.com>
2023-08-13 21:47:33 +0900

    [BE] Feat : Logserver init
    Former-commit-id: 0f2500551346db323087d08ad10e74c34e10a810

---
+++ back/fisher-log-server/src/main/java/com/example/fisherlogserver/kafka/dto/LogDto.java
@@ -0,0 +1,14 @@
+package com.example.fisherlogserver.kafka.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class LogDto {
+    private String sender;
+    private String content;
+}
---
+++ back/fisher-log-server/src/main/java/com/example/fisherlogserver/kafka/service/KafkaConsumer.java
@@ -0,0 +1,24 @@
+package com.example.fisherlogserver.kafka.service;
+
+import com.example.fisherlogserver.kafka.dto.LogDto;
+import com.example.fisherlogserver.kafka.dto.LogDto2;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "${message.topic.name}", groupId = ConsumerConfig.GROUP_ID_CONFIG, containerFactory = "logDtoListener")
+    public void consume(LogDto message) throws IOException{
+        System.out.println("Consumed msg : "+message.toString());
+    }
+
+    @KafkaListener(topics = "${message.topic.name2}", groupId = ConsumerConfig.GROUP_ID_CONFIG, containerFactory = "logDto2Listener")
+    public void consume2(LogDto2 message) throws IOException{
+        System.out.println("Consumed msg : "+message.toString());
+    }
+
+}
