e388c8fe6ea2ef2746ff342ed9326107af5f111b
조병철 <zxas1245@naver.com>
2023-08-14 14:44:20 +0900

    [FE] Feat: 도감 백엔드 데이터 처리
    백엔드에서 받아오는 도감 데이터 + 사용자 데이터 처리 프론트 페이지
    구성완료.. css 수정 및 디자인 필요.
    
    
    Former-commit-id: c3ebaf719af4d859a554b1db61fd5414b7984e29

--- front/src/components/dogam/FishDetailModal.jsx
+++ front/src/components/dogam/FishDetailModal.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./FishDetailModal.css";
 
-const FishDetailModal = ({ fishData, onClose }) => {
+const FishDetailModal = ({ fishData, userFishData, onClose }) => {
   const { fishId, imgUrl, name, info } = fishData;
+
+  console.log(userFishData);
 
   return (
     <div className="fish-detail-modal container">
@@ -10,6 +12,13 @@ const FishDetailModal = ({ fishData, onClose }) => {
         <img src={process.env.REACT_APP_BACKEND_URL + imgUrl} alt={name} />
         <h3>{name}</h3>
         <p>{info}</p>
+        {userFishData ? (
+          <div>
+            <p>최대 크기 : {userFishData.maxSize}</p>
+            <p>잡은 날짜 : {userFishData.getDate}</p>
+            <p>잡은 횟수 : {userFishData.number}</p>
+          </div>
+        ) : null}
         <button onClick={onClose}>닫기</button>
       </div>
     </div>
