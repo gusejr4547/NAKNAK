077777a581df9f099af9333f0230d884d0f02983
jair9002 <cwoh9002@naver.com>
2023-08-16 01:04:45 +0900

    [OPS] jenkins test
    Former-commit-id: 450e5ac9cb50593689db22c50816de4eaaa947bd

--- front/src/components/account/Login.js
+++ front/src/components/account/Login.js
@@ -324,7 +324,7 @@ function Login(props) {
           // href={KAKAO_AUTH_URL}
           href="api1/oauth2/authorization/google"
         >
-          <p>92122</p>
+          <p>922</p>
         </a>
       </div>
     </div>
