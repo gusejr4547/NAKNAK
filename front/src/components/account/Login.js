2237c3577a11c6b44c5333e4a84c82140d7ca985
maknoon9002 <>
2023-08-16 10:01:18 +0900

    t
    Former-commit-id: c69f53f8b3238aacbd92a1d2d2b15ce2f2e78426

--- front/src/components/account/Login.js
+++ front/src/components/account/Login.js
@@ -324,7 +324,7 @@ function Login(props) {
           // href={KAKAO_AUTH_URL}
           href="api1/oauth2/authorization/google"
         >
-          <p>52</p>
+          <p>562</p>
         </a>
       </div>
     </div>
