4e64a7259cd79d4d7d1f8d7727e3ba3a77796866
윤자현 <bajh323@naver.com>
2023-08-16 13:58:12 +0900

    [FE] Fix : 파일선택
    Former-commit-id: 2d1901bc307f6c7a9290e3959402099c857edb92

--- front/src/api/KMAAPI.js
+++ front/src/api/KMAAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "/api5",
   params: {
-    ServiceKey: process.env.REACT_APP_APIKEY,
+    ServiceKey: process.env.REACT_APP_KMA_APIKEY,
     pageNo: "1",
     numOfRows: "14",
     dataType: "JSON",
