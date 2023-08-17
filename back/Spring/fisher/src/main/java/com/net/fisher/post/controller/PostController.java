ff0957555293a3aca0a6cb7a62cb58a1f711d7de
maknoon9002 <>
2023-08-17 14:20:21 +0900

    [OPS] import error
    Former-commit-id: 2e6b76a02d032d346b1024a251c3dc779ddada6d

--- back/Spring/fisher/src/main/java/com/net/fisher/post/controller/PostController.java
+++ back/Spring/fisher/src/main/java/com/net/fisher/post/controller/PostController.java
@@ -28,6 +28,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
